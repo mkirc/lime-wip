4f5dea1aaa5b5fb33fe98842c6b8bdbe994c0580
mkirc <mkirchner@ph2.uni-koeln.de>
2023-10-23 16:42:41 +0200

    Corrects hdf5 lime version

--- inFileTest.bash
+++ inFileTest.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -v ./lime-1.9.5:/lime -v ./models/inFileTest:/model lime-dev lime-run
+docker run -it -v ./lime-1.9.5.hdf5~patch:/lime -v ./models/inFileTest:/model lime-dev lime-run
