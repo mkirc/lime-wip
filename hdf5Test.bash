32b392f50b8343be278f7f600001ad6828ed8e53
mkirc <m.p.kirchner@gmx.de>
2023-12-04 20:29:28 +0100

    adds pass through of cli arguments

--- hdf5Test.bash
+++ hdf5Test.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -it -v ./../lime:/lime -v ./models/hdf5:/model lime-dev lime-run --hdf5 --no-ncurses ./model.c
+docker run -it \
+    -v ./../lime:/lime \
+    -v ./models/hdf5:/model \
+    lime-dev \
+    lime-run --hdf5 --no-ncurses "$@" ./model.c
